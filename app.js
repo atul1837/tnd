cfbfca8403f8a2883f51ff22d698e3226ac210de
Atul Thakur <atul.thakur1837@gmail.com>
2020-04-01 15:18:45 +0530

    added header and footer to login page and included ejs library in project

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-
+app.set("view engine", "ejs");
 app.get("/", function(req, res) {
   console.log("get request to '/'");
   res.send("hello world");
+});
+app.get("/user/login", function(req, res) {
+  res.render("login");
 });
 
 app.listen(3000, function() {
